fee4ba5a3dcea9aa90c77cb9de34a8cd2fa0e07e
Steven Brown <stevenbrown@MacBookAir.lan>
2025-06-13 16:17:37 -0600

    added call to api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,24 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default async function Home() {
+  const data = await fetch('https://randomuser.me/api/?results=5');
+  const json = await data.json();
+  console.log(json)
+  if(!json) return null;
   return (
     <>
       <div className="page">
         <div className="maing">
-          <h1>Member Directory</h1>
+          {json.results.map(r => {
+       
+              return <div key={r.cell}>
+                <p >{r.name.first} { r.name.last}</p>
+                <Image src={r.picture.thumbnail} width={100} height={100} alt="image"/>
+
+               </div>
+          
+          })}
         </div>
       </div>
     </>
